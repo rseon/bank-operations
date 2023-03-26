b0aa6a33bc3963248e10cfe55c581730f1cf46b6
rseon <remi.seon@gmail.com>
2023-03-26 19:50:54 +0200

    fixed accents

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -8,7 +8,7 @@ export const CHECKBOX_STATES = {
 
 export const downloadFile = (content, filename) => {
 	const link = document.createElement("a")
-	link.setAttribute("href", encodeURI(content))
+	link.setAttribute("href", content)
 	link.setAttribute("download", filename)
 	document.body.appendChild(link)
 
