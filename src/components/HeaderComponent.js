88254e1ed68aa45d1a71181787a9ad42585c252c
rseon <remi.seon@gmail.com>
2023-03-31 12:25:58 +0200

    prepare new page

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,4 +1,8 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+
 export default function HeaderComponent() {
+	const router = useRouter()
 
 	const showModal = () => {
 		const { Modal } = require("bootstrap")
@@ -9,17 +13,25 @@ export default function HeaderComponent() {
 	return (
 		<>
 			<div className="container">
-				<header className="d-flex flex-wrap py-3 mb-4 border-bottom">
-					<span className="me-md-auto fs-4">
+				<header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+					<span className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none fs-4">
 						🏦 Bank operations
 					</span>
-					<ul className="nav">
+					<ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
 						<li>
+							<Link href="/" className={`nav-link px-2 ${router.pathname === '/' ? 'link-primary' : 'link-secondary'}`}>Operations</Link>
+						</li>
+						<li>
+							<Link href="/graphs" className={`nav-link px-2 ${router.pathname === '/graphs' ? 'link-primary' : 'link-secondary'}`}>Graphs</Link>
+						</li>
+					</ul>
+					<div className="col-md-3 text-end">
+						{router.pathname === '/' &&
 							<button className="btn btn-outline-primary" onClick={showModal}>
 								➕ New operation
 							</button>
-						</li>
-					</ul>
+						}
+					</div>
 				</header>
 			</div>
 		</>
---
+++ src/pages/graphs.js
@@ -0,0 +1,13 @@
+import HeaderComponent from "@/components/HeaderComponent"
+
+export default function Graphs() {
+	return (
+		<>
+			<HeaderComponent />
+
+			<div className="container">
+				Hello world!
+			</div>
+		</>
+	)
+}
