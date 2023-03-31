71b1237961bfc4bed26f6f0693600abdd19fca66
rseon <remi.seon@gmail.com>
2023-03-31 14:53:05 +0200

    install chartjs

--- src/pages/graphs.js
+++ src/pages/graphs.js
@@ -1,6 +1,23 @@
 import HeaderComponent from "@/components/HeaderComponent"
+import {useMemo} from "react";
+import {getOperationData} from "@/helpers/operation";
 
 export default function Graphs() {
+	const data = useMemo(() => {
+		const { operations } = getOperationData()
+
+		const byRecipient = {}
+		operations.forEach(op => {
+			if (!(op.recipient in byRecipient)) {
+				byRecipient[op.recipient] = []
+			}
+			byRecipient[op.recipient].push(op.amount)
+
+		})
+
+		console.log(byRecipient)
+	}, [])
+
 	return (
 		<>
 			<HeaderComponent />
